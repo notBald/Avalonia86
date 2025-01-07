80d8de4fbfd0f55456b9de5ba7dcd8644f948453
Anders Simonsen <notbald@gmail.com>
2025-01-07 16:04:37 +0100

    Updated AssemblyInfo

--- 86BoxManager/Properties/AssemblyInfo.cs
+++ 86BoxManager/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("86Box Manager")]
-[assembly: AssemblyDescription("A configuration manager for 86Box emulator")]
+[assembly: AssemblyTitle("Avalonia86")]
+[assembly: AssemblyDescription("A configuration manager for the 86Box emulator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Simunič and others")]
 [assembly: AssemblyProduct("86Box Manager")]
-[assembly: AssemblyCopyright("Copyright © 2018-2023 David Simunič and others")]
+[assembly: AssemblyCopyright("Copyright © 2018-2025 David Simunič and others")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.7.6.0")]
-[assembly: AssemblyFileVersion("1.7.6.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
