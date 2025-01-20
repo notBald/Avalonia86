539428af39ffa2aabc52c2a814eb11513e972782
notBald <notbald@gmail.com>
2025-01-20 07:59:48 +0100

    Replaces canvas with a grip control.

---
+++ 86BoxManager/Views/dlgUpdater.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace _86BoxManager.Views;
+
+public partial class dlgUpdater : Window
+{
+    public dlgUpdater()
+    {
+        InitializeComponent();
+    }
+}
