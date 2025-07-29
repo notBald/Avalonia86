2a7ce6ebf53b2b906b1ac20dbe2956a28a8c8876
Anders Simonsen <notbald@gmail.com>
2025-07-30 00:02:31 +0200

    Responsive UI. Issue #28

---
+++ Avalonia86/Converters/HeightToIsVisibleConverter.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Avalonia86.Converters;
+
+/// <summary>
+/// Usage: 
+/// 
+/// <Window.Resources>
+///   <local:HeightToIsVisibleConverter x:Key="HeightToIsVisibleConverter"/>
+/// </Window.Resources>
+/// 
+/// Then: 
+/// <local:MyUserControl IsVisible = "{Binding #MainWindow.Height,
+///                                    Converter={StaticResource HeightToIsVisibleConverter},
+///                                    ConverterParameter = 500}" />
+/// </summary>
+public class HeightToIsVisibleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double height && double.TryParse(parameter?.ToString(), out double threshold))
+        {
+            return height >= threshold;
+        }
+        return true;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
